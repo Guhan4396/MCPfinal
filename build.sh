639c86292d6a1f42ae7235ea0b83abc21c915e5f
Guhan Senthil Subramanian <guhansenthil.subramanian@trustrace.com>
2025-03-27 07:29:30 +0530

    Update build script to use Conda Forge through Miniforge

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-# Install Python dependencies
-python -m pip install --upgrade pip
-pip install -r requirements.txt
+# Download and install Miniforge
+wget "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh" -O miniforge.sh
+chmod +x miniforge.sh
+bash ./miniforge.sh -b -p $HOME/conda
+
+# Add conda to path
+export PATH="$HOME/conda/bin:$PATH"
+
+# Create and activate conda environment
+conda create -n myenv python=3.9 -y
+source $HOME/conda/bin/activate myenv
+
+# Install Python dependencies using conda
+conda install -y pandas=2.0.3 numpy=1.24.3 scikit-learn=1.3.0 -c conda-forge
 
 # Install Node dependencies and build Next.js
 pnpm install
